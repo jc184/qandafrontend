49b6063b63c1831ade9680000f86489f401ffb4a
james chalmers <james.chalmers184@gmail.com>
2021-10-20 17:31:45 +0100

    chapter 6 updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { fontFamily, fontSize, gray2 } from './Styles';
 import logo from './logo.svg';
-import './App.css';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
+
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="questions/:questionId" element={<QuestionPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/HomePage.tsx
@@ -0,0 +1,47 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+import { PrimaryButton } from './Styles';
+import { useNavigate } from 'react-router-dom';
+
+export const HomePage = () => {
+  const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+  React.useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  const [questionsLoading, setQuestionsLoading] = React.useState(true);
+  const navigate = useNavigate();
+  const handleAskQuestionClick = () => {
+    navigate('ask');
+  };
+  return (
+    <Page>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        <PageTitle>Unanswered Questions</PageTitle>
+        <PrimaryButton onClick={handleAskQuestionClick}>
+          Ask a question
+        </PrimaryButton>
+      </div>
+      {questionsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <QuestionList data={questions} />
+      )}
+    </Page>
+  );
+};
